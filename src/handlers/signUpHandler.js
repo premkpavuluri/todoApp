fa66cfcfcfc8530a9259c196fc67147ac2425b6e
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-22 13:37:27 +0530

    Added styling to login and signup pages.

--- src/handlers/signUpHandler.js
+++ src/handlers/signUpHandler.js
@@ -1,4 +1,8 @@
 const signUpPage = (templates) => (req, res) => {
+  if (req.session.isPopulated) {
+    return res.redirect('/todo/home');
+  }
+
   if (req.query.invalid === 'true') {
     return res.type('html').end(templates.signUpWithError);
   }
